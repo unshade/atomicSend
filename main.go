346082ee4065cc200b9cf08547a70c96ead8fbec
unshade <72690981+unshade@users.noreply.github.com>
2024-03-06 08:47:23 +0100

    FEAT : v1 api

--- main.go
+++ main.go
@@ -7,13 +7,15 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	engine := gin.Default()
+	v1engine := engine.Group("/api/v1")
+
+	v1engine.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.POST("/upload-file", func(c *gin.Context) {
+	v1engine.POST("/upload-file", func(c *gin.Context) {
 		file, _ := c.FormFile("file")
 		c.SaveUploadedFile(file, file.Filename)
 		c.JSON(http.StatusOK, gin.H{
@@ -21,5 +23,5 @@ func main() {
 		})
 
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	engine.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
